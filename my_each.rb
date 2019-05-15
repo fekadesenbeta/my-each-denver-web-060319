0262416d12309ef899e7cdf22d5371a3fd9b2652
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-15 21:08:27 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,28 +1,8 @@
-
 def my_each(array)
-  i = 0
-   while i > array.length  
-    yield array([i])
-     i += 1
-    end
-  array
+  i = 0 
+  while i < array.length
+  yield(array[i])
+  i += 1
 end
-
-
-
-
-
-
-
-
-
-
-
-def my_each(array)
-  i = 0
-   while i > array.length  
-    yield array([i])
-     i += 1
-    end
-  array
-end
+array
+end 	end
